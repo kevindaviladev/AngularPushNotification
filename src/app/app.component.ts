53f75a8cb231cbfb2a22c8a739ba3b08dfb673c6
kevin <kfariid@gmail.com>
2021-02-06 23:01:58 -0500

    App de prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+import { PushService } from './push.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'push';
+
+  constructor(private swPush  : SwPush ){
+
+  }
+
+  subscribeNotification(){
+    this.swPush.requestSubscription({
+      serverPublicKey: "BNNWctVklwSAxXao5q74XR-gNsD0OMjqSnAqVeb9_f-nbzkJptsd_ZISPVRdY5D5Rts-23gy9k5kI6AVc1bnZmM"
+    }).then( sub => {
+      console.log(sub.toJSON());
+    })
+  }
 }
